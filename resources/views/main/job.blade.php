0f45f4145021252a234ad8b8a270ea498002cb82
Vladislav Tymoshchuk <tvladislav0504@gmail.com>
2023-06-10 11:26:45 +0300

    Revert "Update 1.2.1.1"
    Виправлено баги

--- resources/views/main/job.blade.php
+++ resources/views/main/job.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 @section('links')
     <link rel="stylesheet" href="{{ asset('css/jobs.css') }}" type="text/css"/>
+    <link rel="stylesheet" href="{{ asset('css/pop-up.css') }}" type="text/css"/>
 @endsection
 
 @section('title')
@@ -14,38 +15,30 @@
                 <h1 class="main-block__title">Доступні вакансії</h1>
                 <section class="about-posts-section">
                     <div class="row">
-
+                            @foreach($jobs as $job)
                         <div class="about-posts about-post" data-aos="fade-right">
                             <div class="about-post-thumbnail-wrapper img__border">
-                                <img src="public/img/jobs/foto1.jpg" alt="blog post">
+                                <img src="{{ 'storage/' . $job->preview_image }}" alt="blog post">
                             </div>
-                            <p class="about-post-category">Викладач математики</p>
-                            <a class="about-post-permalink">
+                            <p class="about-post-category">{{ $job->title }}</p>
+                            <a class="about-post-permalink" href="#popup_{{ $job->id }}">
                                 <h6 class="about-post-title">Детальніше</h6>
                             </a>
                         </div>
-                        <div class="about-posts about-post" data-aos="fade-right">
-                            <div class="about-post-thumbnail-wrapper img__border">
-                                <img src="public/img/jobs/foto2.png" alt="blog post">
+                            <div id="popup_{{ $job->id }}" class="overlay">
+                                <div class="popup">
+                                    <h2>{{ $job->title }}</h2>
+                                    <a class="close" href="#">&times;</a>
+                                    <div class="content" style="margin-top: 25px">
+                                        {!! nl2br(htmlspecialchars_decode($job->content)) !!}
+                                    </div>
+                                </div>
                             </div>
-                            <p class="about-post-category">Викладач программування</p>
-                            <a class="about-post-permalink">
-                                <h6 class="about-post-title">Детальніше</h6>
-                            </a>
-                        </div>
-                        <div class="about-posts about-post" data-aos="fade-right">
-                            <div class="about-post-thumbnail-wrapper img__border">
-                                <img src="public/img/jobs/foto3.jpg" alt="blog post">
-                            </div>
-                            <p class="about-post-category">Бухгалтер</p>
-                            <a class="about-post-permalink">
-                                <h6 class="about-post-title">Детальніше</h6>
-                            </a>
-                        </div>
-
+                        @endforeach
 
                     </div>
                 </section>
+
             </div>
 
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -101,4 +104,15 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', function () {
+    if (Auth::check()) {
+        $user = Auth::user();
+        if ($user->role == User::ROLE_ADMIN) {
+            return redirect()->route('admin.main.index');
+        }
+    }
+
+    $homeController = new HomeController();
+    return $homeController->index();
+})->name('home');
+
