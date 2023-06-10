e87fe616c8faaaf1344bc8d9df63aafa1a5b6b2c
Vladislav Tymoshchuk <tvladislav0504@gmail.com>
2023-06-10 11:22:32 +0300

    Revert "Update 1.2"
    This reverts commit

--- app/Http/Controllers/Main/AboutPageController.php
+++ app/Http/Controllers/Main/AboutPageController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use App\Models\PostAbout;
 use Illuminate\Http\Request;
 
 class AboutPageController extends Controller
 {
     public function __invoke()
     {
-        $posts = PostAbout::paginate(6);
-        return view('main.about', compact('posts'));
+        return view('main.about');
     }
 }
